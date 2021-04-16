4d5097cf2af350691a551cd8ab1ab0d70fc48700
Mike S <Michael.Saelens@healthcaresource.com>
2021-04-16 15:35:18 -0400

    Use modal to add mains and sides.

---
+++ src/AddItemModal.tsx
@@ -0,0 +1,118 @@
+import React, {Component} from "react";
+import Rodal from "rodal";
+import 'rodal/lib/rodal.css';
+import './Modal.scss';
+import {API, graphqlOperation, Storage} from "aws-amplify";
+import {createItem} from './graphql/mutations';
+import ImageComponent from "./ImageComponent";
+import {Container, FormGroup} from "react-bootstrap";
+import {v4 as uuid} from "uuid";
+import config from "./aws-exports";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCameraRetro} from "@fortawesome/free-solid-svg-icons/faCameraRetro";
+
+export interface IAddItemModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    onOK: () => void;
+    category: string;
+    userName: string;
+}
+
+interface IAddItemModalState {
+    file: any;
+    itemName: string;
+}
+
+const {
+    aws_user_files_s3_bucket_region: region,
+    aws_user_files_s3_bucket: bucket
+} = config;
+
+export default class AddItemModal extends Component<IAddItemModalProps, IAddItemModalState> {
+    constructor(props: IAddItemModalProps) {
+        super(props);
+        this.state = {
+            file: null,
+            itemName: ''
+        };
+    }
+
+    async componentDidMount() {
+        this.setState({itemName: ''});
+    }
+
+    async addItem() {
+        const file = this.state.file;
+        if (file) {
+            // @ts-ignore
+            const extension = file.name.split(".")[1];
+            // @ts-ignore
+            const { type: mimeType } = file;
+            const key = `images/${uuid()}${this.props.userName}-${this.state.itemName}.${extension}`;
+            const url = `https://${bucket}.s3.${region}.amazonaws.com/public/${key}`;
+            const inputData = {
+                name: this.state.itemName,
+                category: this.props.category,
+                image: url,
+                userName: this.props.userName
+            };
+
+            try {
+                await Storage.put(key, file, {
+                    contentType: mimeType
+                });
+                await API.graphql(graphqlOperation(createItem, { input: inputData }))
+            } catch (error) {
+                console.log('error: ', error);
+            }
+        }
+        this.props.onOK();
+    }
+
+    handleChange(event: any) {
+        const { target: { value, files } } = event;
+        const fileForUpload = files[0];
+
+        this.setState({
+            itemName: fileForUpload.name.split(".")[0],
+            file: fileForUpload || value
+        });
+    }
+
+    render() {
+        return (
+            <Rodal
+                visible={this.props.isOpen}
+                onClose={() => this.props.onClose()}
+                animation="slideUp"
+                duration={1000}
+                measure="%"
+                width={100}
+                height={88}
+            >
+                <div className="spacer"/>
+                <Container className="planner-modal">
+                    <FormGroup>
+                        <Row>
+                            <Col className="col-3 img-col">
+                                <input type="file" name="file" id="file" className="input-file" onChange={event => this.handleChange(event)} accept="image/png, image/jpeg"/>
+                                <label htmlFor="file">
+                                    <FontAwesomeIcon className="link-icon" icon={faCameraRetro}/>
+                                </label>
+                            </Col>
+                            <Col className="col-7 px-2 my-auto">
+                                <input type="text" placeholder="Name" className="" value={this.state.itemName} onChange={e => this.setState({itemName: e.target.value})}/>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    <FormGroup className="text-center">
+                        <button className="btn btn-primary btn-on-bottom" onClick={() => this.addItem()}>Add</button>
+                    </FormGroup>
+                </Container>
+            </Rodal>
+        )
+    }
+}
--- src/Mains.tsx
+++ src/Mains.tsx
@@ -4,29 +4,23 @@ import './MainsAndSides.scss';
 import { Container, FormGroup } from 'react-bootstrap';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {Storage, API, graphqlOperation, Auth} from 'aws-amplify';
-import { v4 as uuid } from 'uuid';
-import { createItem, deleteItem } from './graphql/mutations';
-import config from './aws-exports';
+import {API, graphqlOperation, Auth} from 'aws-amplify';
+import { deleteItem } from './graphql/mutations';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faCameraRetro } from '@fortawesome/free-solid-svg-icons/faCameraRetro';
 import { faMinusCircle } from '@fortawesome/free-solid-svg-icons/faMinusCircle';
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons/faPlusCircle';
 import { faEdit as editIcon } from '@fortawesome/free-solid-svg-icons/faEdit';
 import * as queries from './graphql/queries';
 import ImageComponent from "./ImageComponent";
-
-const {
-    aws_user_files_s3_bucket_region: region,
-    aws_user_files_s3_bucket: bucket
-} = config;
+import {faPlusCircle} from "@fortawesome/free-solid-svg-icons/faPlusCircle";
+import AddItemModal from "./AddItemModal";
 
 export default class Mains extends Component {
     state = {
         file: null,
         itemName: '',
         items: [],
-        userName: ''
+        userName: '',
+        modalIsOpen: false
     };
 
     async componentDidMount() {
@@ -39,15 +33,6 @@ export default class Mains extends Component {
         }
     }
 
-    handleChange(event: any) {
-        const { target: { value, files } } = event;
-        const fileForUpload = files[0];
-
-        this.setState({
-            itemName: fileForUpload.name.split(".")[0],
-            file: fileForUpload || value
-        });
-    }
 
     async listItems() {
         const filter = {
@@ -72,40 +57,36 @@ export default class Mains extends Component {
         }
     }
 
-    async addItem() {
-        const file = this.state.file;
-        if (file) {
-            // @ts-ignore
-            const extension = file.name.split(".")[1];
-            // @ts-ignore
-            const { type: mimeType } = file;
-            const key = `images/${uuid()}${this.state.userName}-${this.state.itemName}.${extension}`;
-            const url = `https://${bucket}.s3.${region}.amazonaws.com/public/${key}`;
-            const inputData = {
-                name: this.state.itemName,
-                category: 'Mains',
-                image: url,
-                userName: this.state.userName
-            };
+    onOpenModal() {
+        this.setState({modalIsOpen: true});
+    }
 
-            try {
-                await Storage.put(key, file, {
-                    contentType: mimeType
-                });
-                await API.graphql(graphqlOperation(createItem, { input: inputData }))
-                await this.listItems();
-            } catch (error) {
-                console.log('error: ', error);
-            }
-        }
+    onCloseModal() {
+        this.setState({modalIsOpen: false});
+    }
+
+    async onItemAdded() {
+        this.onCloseModal();
+        await this.listItems();
     }
 
     render() {
         return (
             <div className="page">
+                <AddItemModal category="Mains" isOpen={this.state.modalIsOpen} onOK={() => this.onItemAdded()} onClose={() => this.onCloseModal()} userName={this.state.userName}/>
                 <TopNavbar title="Mains" showBackNav={true}/>
                 <Container>
                     <FormGroup>
+                        <Row onClick={() => this.onOpenModal()}>
+                            <Col className="col-3">
+                                <div className="add-item-placeholder">
+                                    <FontAwesomeIcon className="link-icon" icon={faPlusCircle}/>
+                                </div>
+                            </Col>
+                            <Col className="col-7 px-2 my-auto">
+                                <div className="text-md-left">Add...</div>
+                            </Col>
+                        </Row>
                         {
                             this.state.items.map((item: any) => (
                                 <Row key={item.id}>
@@ -124,22 +105,6 @@ export default class Mains extends Component {
                                 </Row>
                             ))
                         }
-                        <Row>
-                            <Col className="col-3 img-col">
-                                <input type="file" name="file" id="file" className="input-file" onChange={event => this.handleChange(event)} accept="image/png, image/jpeg"/>
-                                <label htmlFor="file">
-                                    <FontAwesomeIcon className="link-icon" icon={faCameraRetro}/>
-                                </label>
-                            </Col>
-                            <Col className="col-7 px-2 my-auto">
-                                <input type="text" placeholder="Name" className="" value={this.state.itemName} onChange={e => this.setState({itemName: e.target.value})}/>
-                            </Col>
-                            <Col className="col-1 px-0 my-auto">
-                                <FontAwesomeIcon className="link-icon" icon={faPlusCircle} onClick={() => this.addItem()}/>
-                            </Col>
-                            <Col className="col-1 px-0 my-auto">
-                            </Col>
-                        </Row>
                     </FormGroup>
                     <div className="spacer"/>
                 </Container>
