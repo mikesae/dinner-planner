ca4ed89a1e3332ffda99f47bdfc5ea45fdca46b6
mikesae <msaelens@gmail.com>
2022-12-12 16:04:43 -0500

    Upgrade to latest aws-amplify and aws-amplify/ui-react

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import TopNavbar from './TopNavbar';
 import './Profile.scss';
 import { Container, FormGroup } from 'react-bootstrap';
-import {AmplifySignOut} from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import { Auth } from 'aws-amplify';
 
 export default class Profile extends Component {
@@ -14,13 +14,13 @@ export default class Profile extends Component {
     componentDidMount() {
         Auth.currentAuthenticatedUser({
             bypassCache: true
-        }).then(user => this.setState({userName: user.username, email: user.attributes.email}));
+        }).then(user => this.setState({ userName: user.username, email: user.attributes.email }));
     }
 
     render() {
         return (
             <div className="page">
-                <TopNavbar title="Profile" showBackNav={true}/>
+                <TopNavbar title="Profile" showBackNav={true} />
                 <Container className="container">
                     <FormGroup>
                         <div className="form-label">Username: <b>{this.state.userName}</b></div>
@@ -28,10 +28,14 @@ export default class Profile extends Component {
                     <FormGroup>
                         <div className="form-label">Email: <b>{this.state.email}</b></div>
                     </FormGroup>
-                    <FormGroup>
-                        <AmplifySignOut/>
-                    </FormGroup>
-                    <div className="spacer"/>
+                    <Authenticator>
+                        {({ signOut }) => (
+                            <FormGroup>
+                                <button onClick={signOut}>Sign out</button>
+                            </FormGroup>
+                        )}
+                    </Authenticator>
+                    <div className="spacer" />
                 </Container>
             </div>
         )
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
 //import reportWebVitals from './reportWebVitals';
 
 Amplify.configure(config);
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
