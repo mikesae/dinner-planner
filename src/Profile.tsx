42fe70f71af664279fe63b0819be3c561149bc0f
mikesae <mikesae87@gmail.com>
2023-10-26 16:04:34 -0400

    Prettify.

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -6,38 +6,44 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import { Auth } from 'aws-amplify';
 
 export default class Profile extends Component {
-    state = {
-        userName: '',
-        email: ''
-    };
+	state = {
+		userName: '',
+		email: '',
+	};
 
-    componentDidMount() {
-        Auth.currentAuthenticatedUser({
-            bypassCache: true
-        }).then(user => this.setState({ userName: user.username, email: user.attributes.email }));
-    }
+	componentDidMount() {
+		Auth.currentAuthenticatedUser({
+			bypassCache: true,
+		}).then((user) =>
+			this.setState({ userName: user.username, email: user.attributes.email })
+		);
+	}
 
-    render() {
-        return (
-            <div className="page">
-                <TopNavbar title="Profile" showBackNav={true} />
-                <Container className="container">
-                    <FormGroup>
-                        <div className="form-label">Username: <b>{this.state.userName}</b></div>
-                    </FormGroup>
-                    <FormGroup>
-                        <div className="form-label">Email: <b>{this.state.email}</b></div>
-                    </FormGroup>
-                    <Authenticator>
-                        {({ signOut }) => (
-                            <FormGroup>
-                                <button onClick={signOut}>Sign out</button>
-                            </FormGroup>
-                        )}
-                    </Authenticator>
-                    <div className="spacer" />
-                </Container>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className='page'>
+				<TopNavbar title='Profile' showBackNav={true} />
+				<Container className='container'>
+					<FormGroup>
+						<div className='form-label'>
+							Username: <b>{this.state.userName}</b>
+						</div>
+					</FormGroup>
+					<FormGroup>
+						<div className='form-label'>
+							Email: <b>{this.state.email}</b>
+						</div>
+					</FormGroup>
+					<Authenticator>
+						{({ signOut }) => (
+							<FormGroup>
+								<button onClick={signOut}>Sign out</button>
+							</FormGroup>
+						)}
+					</Authenticator>
+					<div className='spacer' />
+				</Container>
+			</div>
+		);
+	}
 }
