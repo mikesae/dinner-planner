5fd9ebd7dcf18ba1d13e64d5933686edc0a37346
mikesae <mikesae87@gmail.com>
2023-11-27 14:04:49 -0500

    Convert Profile to function component; Add test.

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,51 +1,44 @@
 import { Authenticator } from '@aws-amplify/ui-react';
-import { Auth } from 'aws-amplify';
-import { Component } from 'react';
+import { FunctionComponent, useContext } from 'react';
 import { Container, FormGroup } from 'react-bootstrap';
+import AuthenticatedUserContext from './AuthenticatedUserContext';
 import './Profile.scss';
 import TopNavbar from './TopNavbar';
 
-export default class Profile extends Component {
-	state = {
-		userName: '',
-		email: '',
-	};
+const Profile: FunctionComponent = () => {
+  const user = useContext<any>(AuthenticatedUserContext);
 
-	componentDidMount() {
-		Auth.currentAuthenticatedUser({
-			bypassCache: true,
-		}).then((user) => this.setState({ userName: user.username, email: user.attributes.email }));
-	}
+  return (
+    <div className='page'>
+      <TopNavbar title='Profile' showBackNav={true} />
+      {user && (
+        <Container className='container'>
+          <FormGroup>
+            <div className='form-label'>
+              Username: <b>{user.username}</b>
+            </div>
+          </FormGroup>
+          <FormGroup>
+            <div className='form-label'>
+              Email: <b>{user.attributes.email}</b>
+            </div>
+          </FormGroup>
+          <Authenticator>
+            {({ signOut }) => (
+              <FormGroup>
+                <button
+                  className='amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth'
+                  onClick={signOut}
+                >
+                  Sign out
+                </button>
+              </FormGroup>
+            )}
+          </Authenticator>
+        </Container>
+      )}
+    </div>
+  );
+};
 
-	render() {
-		return (
-			<div className='page'>
-				<TopNavbar title='Profile' showBackNav={true} />
-				<Container className='container'>
-					<FormGroup>
-						<div className='form-label'>
-							Username: <b>{this.state.userName}</b>
-						</div>
-					</FormGroup>
-					<FormGroup>
-						<div className='form-label'>
-							Email: <b>{this.state.email}</b>
-						</div>
-					</FormGroup>
-					<Authenticator>
-						{({ signOut }) => (
-							<FormGroup>
-								<button
-									className='amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth'
-									onClick={signOut}
-								>
-									Sign out
-								</button>
-							</FormGroup>
-						)}
-					</Authenticator>
-				</Container>
-			</div>
-		);
-	}
-}
+export default Profile;
