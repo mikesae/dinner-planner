91b1df3393b82d8e382f7e8c788febf0587c1433
mikesae <mikesae87@gmail.com>
2023-12-04 16:32:24 -0500

    Fix hover and focus colors for top navbar buttons (date picker, profile).

--- src/Planner.tsx
+++ src/Planner.tsx
@@ -12,65 +12,73 @@ import PlannerRow from './PlannerRow';
 import TopNavbar from './TopNavbar';
 
 export interface IPlannerProps {
-	startDate: Date;
-	startDateUpdater: (date: Date) => void;
-	desiredRowCount: number;
+  startDate: Date;
+  startDateUpdater: (date: Date) => void;
+  desiredRowCount: number;
 }
 
 export default class Planner extends Component<IPlannerProps> {
-	CustomInput = React.forwardRef<HTMLInputElement>((props: any, ref: any) => {
-		return (
-			<div className='date-picker' ref={ref}>
-				<button className='btn' onClick={() => this.onPreviousWeek()}>
-					<FontAwesomeIcon className='link-icon fa-1pt5x' icon={faChevronUp} />
-				</button>
-				<span onClick={props.onClick}>
-					&nbsp;{props.value}&nbsp;
-					<button className='btn btn-date-picker'>
-						<FontAwesomeIcon className='link-icon fa-1pt5x' icon={faCalendarAlt} />
-					</button>
-				</span>
-				<button className='btn' onClick={() => this.onNextWeek()}>
-					<FontAwesomeIcon className='link-icon fa-1pt5x' icon={faChevronDown} />
-				</button>
-			</div>
-		);
-	});
+  CustomInput = React.forwardRef<HTMLInputElement>((props: any, ref: any) => {
+    return (
+      <div className='date-picker' ref={ref}>
+        <button className='no-focus' onClick={() => this.onPreviousWeek()}>
+          <FontAwesomeIcon className='link-icon fa-1pt5x' icon={faChevronUp} />
+        </button>
+        <span onClick={props.onClick}>
+          &nbsp;{props.value}&nbsp;
+          <button className='button-date-picker'>
+            <FontAwesomeIcon
+              className='link-icon fa-1pt5x'
+              icon={faCalendarAlt}
+            />
+          </button>
+        </span>
+        <button className='no-focus' onClick={() => this.onNextWeek()}>
+          <FontAwesomeIcon
+            className='link-icon fa-1pt5x'
+            icon={faChevronDown}
+          />
+        </button>
+      </div>
+    );
+  });
 
-	onPreviousWeek() {
-		this.props.startDateUpdater(offsetDate(this.props.startDate, -7));
-	}
+  onPreviousWeek() {
+    this.props.startDateUpdater(offsetDate(this.props.startDate, -7));
+  }
 
-	onNextWeek() {
-		this.props.startDateUpdater(offsetDate(this.props.startDate, 7));
-	}
+  onNextWeek() {
+    this.props.startDateUpdater(offsetDate(this.props.startDate, 7));
+  }
 
-	plannerRows() {
-		return Array.from({ length: this.props.desiredRowCount }, (_, offset) => (
-			<PlannerRow
-				key={offsetDate(this.props.startDate, offset).toDateString()}
-				startDateUpdater={this.props.startDateUpdater}
-				date={offsetDate(this.props.startDate, offset)}
-			/>
-		));
-	}
+  plannerRows() {
+    return Array.from({ length: this.props.desiredRowCount }, (_, offset) => (
+      <PlannerRow
+        key={offsetDate(this.props.startDate, offset).toDateString()}
+        startDateUpdater={this.props.startDateUpdater}
+        date={offsetDate(this.props.startDate, offset)}
+      />
+    ));
+  }
 
-	render() {
-		const startDate = this.props.startDate;
+  render() {
+    const startDate = this.props.startDate;
 
-		return (
-			<div className='page'>
-				<TopNavbar title={''} showBackNav={false}>
-					<DatePicker
-						selected={startDate}
-						onChange={(date: Date) => this.props.startDateUpdater(getPreviousStartDay(date))}
-						customInput={<this.CustomInput />}
-					/>
-				</TopNavbar>
-				<Container>
-					<>{this.plannerRows()}</>
-				</Container>
-			</div>
-		);
-	}
+    return (
+      <div className='page'>
+        <TopNavbar title={''} showBackNav={false}>
+          <DatePicker
+            selected={startDate}
+            onChange={(date: Date) =>
+              this.props.startDateUpdater(getPreviousStartDay(date))
+            }
+            customInput={<this.CustomInput />}
+          />
+        </TopNavbar>
+        <Container>
+          <>{this.plannerRows()}</>
+        </Container>
+      </div>
+    );
+  }
 }
