20d6428309021d82a126a83e9987583027d56954
mikesae <msaelens@gmail.com>
2023-12-26 13:09:15 -0500

    Upgrade to latest react version (18)

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import { Amplify } from 'aws-amplify';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { render } from 'react-dom';
 import config from './aws-exports';
 import AuthenticatedApp from './components/app/AuthenticatedApp';
 import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import ReactDOM from 'react-dom/client';
 
 Amplify.configure(config);
-render(<AuthenticatedApp />, document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<AuthenticatedApp />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
